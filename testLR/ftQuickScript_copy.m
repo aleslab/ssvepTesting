15064caa825214f69f8e03e68921768c8af6434c
Marlene <marlene.poncet@gmail.com>
2017-07-19 17:03:45 +0100

    Add analysis for long-range

---
+++ testLR/ftQuickScript_copy.m
@@ -0,0 +1,100 @@
+% clean data?
+% reference 32=Cz? 16=Oz?
+% 1 frame difference with the photodiode - any readjustment?
+% baseline window? No need? (detrending?)
+% 
+
+
+clear all
+
+% addpath D:\GitHubRepo\fieldtrip
+% ft_defaults
+% cfg.dataset   = 'D:\StAndrews\testSSVEP\SSVEPtest3.bdf';
+
+addpath /Users/marleneponcet/Documents/Git/fieldtrip
+ft_defaults
+cfg.dataset = '/Users/marleneponcet/Documents/ssvep/test/testSSVEP/SSVEPtest3.bdf';
+
+%   cfg.dataset      = string with the filename
+%   cfg.trl          = Nx3 matrix with the trial definition, see FT_DEFINETRIAL
+%   cfg.padding      = length (in seconds) to which the trials are padded for filtering (default = 0)
+%   cfg.padtype      = string, type of padding (default: 'data' padding or
+%                      'mirror', depending on feasibility)
+%   cfg.continuous   = 'yes' or 'no' whether the file contains continuous data
+%                      (default is determined automatic)
+%
+
+cfg.trialdef.bitmask = 2^9-1;%Values to keep.
+cfg.trialdef.condRange = [101 165];
+cfg.trialdef.ssvepTagVal = 1;
+cfg.trialdef.epochLength = 2; 
+cfg.trialfun = 'lock2SsvepTag_copy'; 
+
+[cfg] = ft_definetrial(cfg);
+
+cfg.demean        ='yes';
+cfg.reref         = 'yes';
+cfg.refchannel    = {'A32'}; % 32=Cz?
+[data] = ft_preprocessing(cfg);
+
+% %%% A bit of visualization
+% % plot the same channel over all epochs ( = cycles + trials)
+% figure; hold on;
+% for nbtrials = 1:5%length(data.trial)
+%     test = data.trial{1,nbtrials};
+%     plot(test(1,:),'Color',[rand(1) rand(1) rand(1)])
+% end
+
+cfg = [];
+[timelock] = ft_timelockanalysis(cfg, data); % computes the timelocked average ERP/ERF
+
+% plot the average for a specified channel
+% plot(timelock.avg(1,:))
+
+%%% Power Spectrum
+fftDat = fft(timelock.avg(1:32,:)'); % for the 32 channels
+% spectrumDat = abs(2*fftDat(2:floor(end/2))/length(fftDat));
+spectrumDat = abs(fftDat(2:floor(end/2))); % discard the second half of the signal (since it's symetric). 
+% Also transforms the matrix in a single vector. To keep the channels
+% separate (2D matrix): spectrumDat = abs(fftDat(2:floor(end/2),:));
+spectrumDatAll = spectrumDat*2/length(fftDat); % estimate of the power amplitude = 2*A/N because N sample data amplitude A results in a DFT peak of AN/2
+freqs = [1:length(spectrumDatAll)] * 1/(timelock.time(end)-timelock.time(1));
+figure; hold on; plot(freqs(freqs<80),spectrumDatAll(freqs<80),'r');
+pdSpecPlot_copy(freqs(1:80),spectrumDatAll(1:80),[]);
+
+spectrumChan = abs(fftDat(2:floor(end/2),16)); % for the specified channel, here 16=Oz?
+spectrumChan = spectrumChan*2/length(spectrumChan);
+figure; hold on; plot(freqs(freqs<80),spectrumChan(freqs<80),'r');
+pdSpecPlot_copy(freqs(1:80),spectrumChan(1:80),[]);
+
+
+%%% Below uses dfmtx BUT
+%%%   If X is a column vector of length N, then
+%%%   DFTMTX(N)*X yields the same result as FFT(X); however, 
+%%%   FFT(X) is more efficient.
+% Axx.Wave = timelock.avg(1:32,:)';
+% Axx.nT = size(Axx.Wave,1);
+% Axx.nFr = round(size(Axx.Wave,1)/2);
+% dft = dftmtx(Axx.nT);
+% 
+% dftDat = dft*Axx.Wave;
+% dftDat = dftDat(1:Axx.nFr,:);
+% 
+% Axx.dFHz = (data.hdr.Fs)/Axx.nT;
+% 
+% % Stuff tends to be multiples of 1 Hz. But we don't have that info
+% % here so we are just going to set the index to 1 to make nF1 be all
+% % freqs
+% Axx.i1F1       = 1;
+% Axx.i1F2       = 0;
+% 
+% Axx.Amp = abs(2*(dftDat/Axx.nFr));
+% Axx.Cos = 2*real(dftDat)/Axx.nFr;
+% Axx.Sin = -2*imag(dftDat)/Axx.nFr;
+% 
+% 
+% 
+% freqs = 0:Axx.dFHz:(Axx.dFHz*(Axx.nFr-1));
+% 
+% pdSpecPlot(freqs(1:80),Axx.Amp(1:80,16)',[])
+
---
+++ testLR/lock2SsvepTag_LR.m
@@ -0,0 +1,106 @@
+function trl = lock2SsvepTag_LR(cfg)
+
+hdr   = ft_read_header(cfg.dataset);
+event = ft_read_event(cfg.dataset);
+
+trl = [];
+
+%Silly hard coded numbers
+%
+bitmask = 2^9-1;%Values to keep.
+condRange = [101 165];
+ssvepTagVal = 1;
+epochLength = 2; %approx epoch length in ms.
+numExpectedCycles = 15;
+
+% check if there is any invalid trial
+for i=1:length(event)
+    thisMaskedVal = bitand(event(i).value,bitmask);
+    if thisMaskedVal == 98
+        disp('invalid trials, do something')
+    end
+end
+
+%First find all condition starts
+idx = 1;
+for i=1:length(event)
+    
+    if isempty(event(i).value)
+        continue;
+    end
+    
+    thisMaskedVal = bitand(event(i).value,bitmask);
+    
+    if thisMaskedVal>=condRange(1) && thisMaskedVal<=condRange(2)
+        condEventIdx(idx) = i;
+        condNum(idx)      = thisMaskedVal;
+        idx = idx+1;
+    end
+    
+end
+
+condEventIdx(idx) = length(event)+1; % create a last fake event (necessary to get the last trial included,see the following loop)
+
+numTrial = 1;
+%Now lets break up each condition trial and find all ssvep cycles.
+for iTrial = 1:length(condEventIdx)-1
+    % get the ssvep triggers for this trial 
+    % +1/-1 should remove the condition number, not the experiment start/stop trigger. 
+    % Also -1 necessary for the last trial: the last event does not have a value (that's the fake event created just before the loop)
+    startCond = condEventIdx(iTrial)+1;   
+    endCond = condEventIdx(iTrial+1)-1; 
+    
+    condValues  = [event(startCond:endCond).value]; 
+    condValues  = bitand(condValues,bitmask);
+    condSamples = [event(startCond:endCond).sample];
+    
+    % get only the 'real' ssvep triggers 
+    cycleStarts = condSamples(find(condValues<5));  % include dim flashes
+    numCycles = length(cycleStarts);
+    %     trial(iTrial).cycleStarts = condSamples(condValues==ssvepTagVal); % does not include dim flashes
+    
+    % sanity checks  
+    if  numCycles ~= numExpectedCycles % check for a missing cycle
+        fprintf('\n not the expected number of cycles in trial %d',iTrial)
+    elseif find(abs(diff(diff(cycleStarts))) > 5) % check for a missing frame
+        fprintf('\n wrong duration trial %d',iTrial)
+    else
+        trial(numTrial).cycleStarts = cycleStarts;
+        trial(numTrial).condNum = condNum(iTrial);
+        
+        %     numCycles = length(trial(iTrial).cycleStarts);
+        meanSampPerCycle= mean(diff(trial(numTrial).cycleStarts));
+        numCyclesPerEpoch = floor(epochLength / (meanSampPerCycle/hdr.Fs));
+        
+        for iCycle = 1:numCyclesPerEpoch:numCycles-(numCyclesPerEpoch-1)
+            begsample     = trial(numTrial).cycleStarts(iCycle);
+            endsample     = begsample+numCyclesPerEpoch*meanSampPerCycle;
+            offset        = 0;
+            trl(end+1, :) = round([begsample endsample offset trial(numTrial).condNum]);
+        end
+        numTrial = numTrial + 1;
+    end
+    
+% % THIS INCLUDES BAD STIM DURATION
+%     % sanity checks  
+%     if  numCycles ~= numExpectedCycles 
+%         fprintf('\n not the expected number of cycles in trial %d',iTrial)
+%     else
+%         trial(numTrial).cycleStarts = cycleStarts; % include dim flashes
+%         trial(numTrial).condNum = condNum(iTrial);
+%         
+%         %     numCycles = length(trial(iTrial).cycleStarts);
+%         meanSampPerCycle= 819; % mean(diff(trial(numTrial).cycleStarts));
+%         numCyclesPerEpoch = floor(epochLength / (meanSampPerCycle/hdr.Fs));
+%         
+%         for iCycle = 1:numCyclesPerEpoch:numCycles-(numCyclesPerEpoch-1)
+%             begsample     = trial(numTrial).cycleStarts(iCycle);
+%             endsample     = begsample+numCyclesPerEpoch*meanSampPerCycle;
+%             offset        = 0;
+%             trl(end+1, :) = round([begsample endsample offset trial(numTrial).condNum]);
+%         end
+%         numTrial = numTrial + 1;
+%     end
+    
+end
+
