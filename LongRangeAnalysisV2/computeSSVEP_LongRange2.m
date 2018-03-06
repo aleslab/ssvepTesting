aa03e41847c11ebdabc955b13c7ed095cfb66186
Marlene <marlene.poncet@gmail.com>
2018-03-06 17:54:33 +0000

    average ref data

--- LongRangeAnalysisV2/computeSSVEP_LongRange2.m
+++ LongRangeAnalysisV2/computeSSVEP_LongRange2.m
@@ -1,41 +1,51 @@
 
 % compute steady state and the predictions
 
-clear all;
-addpath /Users/marleneponcet/Documents/Git/fieldtrip20170924
+clearvars;
+addpath /Users/marleneponcet/Documents/Git/fieldtrip-aleslab-fork
 addpath /Users/marleneponcet/Documents/Git/ssvepTesting/svndlCopy
 addpath /Users/marleneponcet/Documents/Git/ssvepTesting/biosemiUpdated
 ft_defaults
 
 dataDir = '/Users/marleneponcet/Documents/dataLongRangeV2/cleanData/';
-listData = dir([dataDir '*.mat']);
-cfg.layout = 'biosemi128.lay';
-cfg.channel =  {'all','-EXG1', '-EXG2', '-EXG3','-EXG4','-EXG5','-EXG6','-EXG7','-EXG8', '-Status'};
-    
-dataOut = '/Users/marleneponcet/Documents/dataLongRangeV2/dataFiles/';
+listData = dir([dataDir '*.mat']);    
+dataOut = '/Users/marleneponcet/Documents/dataLongRangeV2/AxxFilesAverageRef/';
 
 for ff=1:length(listData)
     
-    clear cleanData; clear Axx;
+    clear cleanData; clear Axx; clear cfg;
     load([dataDir listData(ff).name]);
-    
-    % create folder for each subject 
-    newFolder = sprintf('%s', [dataOut listData(ff).name(11:13)]);
-    if ~exist(newFolder, 'dir')
-        mkdir(newFolder);
-    end
-    
+
+    % reref to average
+    cfg.reref         = 'yes';
+    cfg.refchannel    = {'all','-EXG1', '-EXG2', '-EXG3','-EXG4','-EXG5','-EXG6','-EXG7','-EXG8', '-Status'};
+    cfg.channel =  {'all','-EXG1', '-EXG2', '-EXG3','-EXG4','-EXG5','-EXG6','-EXG7','-EXG8', '-Status'};
+    [cleanData] = ft_preprocessing(cfg,cleanData);
+
     % compute steady state
     cleanData=rmfield(cleanData,'cfg'); % clear cfg so that the data does not become too heavy
+    
+%     % hierarchical structure
+%     % create folder for each subject 
+%     newFolder = sprintf('%s', [dataOut listData(ff).name(11:13)]);
+%     if ~exist(newFolder, 'dir')
+%         mkdir(newFolder);
+%     end
+%     allcond = unique(cleanData.trialinfo(:,1));  
+%     for cond=1:length(allcond)
+%         cfg.trials = find(cleanData.trialinfo(:,1) == allcond(cond));
+%         Axx = ft_steadystateanalysis(cfg, cleanData); % the field elec is present only if a channel has been replaced  
+%         save([newFolder '/Axx_' listData(ff).name(11:13) '_c' num2str(cond,'%.2d')],'Axx')
+%     end    
 
+    % 1 Axx per sbj
     allcond = unique(cleanData.trialinfo(:,1));  
     for cond=1:length(allcond)
         cfg.trials = find(cleanData.trialinfo(:,1) == allcond(cond));
-        % TO DO: add a column for trial number + the condition names
-        Axx = ft_steadystateanalysis(cfg, cleanData); % the field elec is present only if a channel has been replaced  
-        save([newFolder '/Axx_' listData(ff).name(11:13) '_c' num2str(cond,'%.2d')],'Axx')
-    end    
-     
+        [Axx.cond(cond)] = ft_steadystateanalysis(cfg, cleanData); % the field elec is present only if a channel has been replaced  
+    end       
+    save([dataOut 'Axx_' listData(ff).name(11:13)],'Axx')
+
 %     % call function for prediction save it in Axx
 %     Axx.predictMotion = predictMotion(Axx.cond);
 %     Axx.predictSimult = predictSimult(Axx.cond);
