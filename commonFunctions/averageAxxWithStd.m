0721af07801cbe561ae1c0655b12aec3ee16d858
Marlene <marlene.poncet@gmail.com>
2018-11-29 16:18:44 +0000

    new Figures (add std)

--- commonFunctions/averageAxxWithStd.m
+++ commonFunctions/averageAxxWithStd.m
@@ -1,5 +1,6 @@
-function [groupAv] = averageAxx(dataSbj)
-
+function [groupAv,sbjProj] = averageAxxWithStd(dataSbj)
+% groupAv contains wave, sin, cos, amp average per condition
+% sbjProj contains projected freq amplitudes for each participant per condtion (can be used to calculated std of amplitudes) 
 
 for cond=1:size(dataSbj,1) 
 % values that do not change
@@ -25,14 +26,25 @@ for cond=1:size(dataSbj,1)
         dataTmpWave(:,:,ss) = [dataSbj(cond,ss).wave];
         dataTmpSin(:,:,ss) = [dataSbj(cond,ss).sin];
         dataTmpCos(:,:,ss) = [dataSbj(cond,ss).cos];
+        dataTmpAmp(:,:,ss) = [dataSbj(cond,ss).amp]; % sbj amplitude
+        dataTmpPhi(:,:,ss) = atan2(dataSbj(cond,ss).cos,dataSbj(cond,ss).sin); % sbj phase
     end
     groupAv(cond).wave = mean(dataTmpWave,3);
     groupAv(cond).sin = mean(dataTmpSin,3);
     groupAv(cond).cos = mean(dataTmpCos,3);
-    groupAv(cond).amp = sqrt(groupAv(cond).sin.^2 + groupAv(cond).cos.^2);
+    groupAv(cond).amp = sqrt(groupAv(cond).sin.^2 + groupAv(cond).cos.^2); % this is just to check that mean in group is the same as mean in the sbjProj
+    groupTMPtheta = atan2(groupAv(cond).cos,groupAv(cond).sin); % phase of mean
+    
+    % projected amplitude for each sbj so that sdt can be computed
+    % = individual subject amplitude projected onto mean vector
+    % = individual participant amplitude scales by the cosine of the angle between the individual participant response and the group average
+    for ss=1:size(dataSbj,2)
+        sbjProj(:,:,cond,ss) = dataTmpAmp(:,:,ss) .* cos( dataTmpPhi(:,:,ss) - groupTMPtheta(:,:)); 
+    end
+    
 end
 
 
-
 end
+
 
