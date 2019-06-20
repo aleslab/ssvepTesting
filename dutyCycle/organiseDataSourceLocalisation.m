464f82d3f552e12ad86c9a1c0f59eac35e6823de
Unknown <marlene.poncet@gmail.com>
2019-06-20 18:44:36 +0100

    include motion axx

--- dutyCycle/organiseDataSourceLocalisation.m
+++ dutyCycle/organiseDataSourceLocalisation.m
@@ -9,6 +9,10 @@ newFolder = 'dataSourceLoc';
 mkdir(newFolder); % create folder in which each sbj folder will be created
 freqCond = {'F10_6','F5_3','F2_7'};
 
+motion(1).cond = 18;
+motion(2).cond = [17 19 22];
+motion(3).cond = [16 20 21];
+
 for freq = 1:3
     % create separate folder for each freq condition
     mkdir([newFolder filesep freqCond{freq}])
@@ -24,32 +28,47 @@ for freq = 1:3
         % load Axx
         load ([dirData 'Axx' filesep listAxx(ff).name])
         
-        % copy wave if it is not the same size as the other conditions
-        for cc=1:length(Axx)
-            sizeAxx(cc) = size(Axx(cc).wave,2);
-        end
-        if unique(sizeAxx) > 1
-            waveMax = max(sizeAxx);
-            for cc=1:length(Axx)
-                if size(Axx(cc).wave,2) ~= waveMax
-                    nbCopy = waveMax / size(Axx(cc).wave,2);
-                    Axx(cc).wave = repmat(Axx(cc).wave,1,nbCopy);
-                    % need to replace nt as well
-                    Axx(cc).nt = waveMax;
-                    % I copy the time or should replace it?
-                    Axx(cc).time = repmat(Axx(cc).time,1,nbCopy);
-                end
-            end
-        end
-        
+
         for cc=1+5*(freq-1):5*freq % do not include moving condition
             clear uniAxx;
+            % double the wave for the single stim condition so that it fits
+            % with the motion condition
+            Axx(cc).wave = repmat(Axx(cc).wave,1,2);
+            Axx(cc).nt = Axx(17+freq).nt; % nt from moving cond
+            Axx(cc).time = Axx(17+freq).time; % time from moving cond
             % reformat Axx to be consistent with mrcDataViewer
             uniAxx = uniformAxx(Axx(cc));
             % save each condition separately
             save([axxFolder filesep 'Axx_c' num2str(cc,'%02d')],'-struct','uniAxx')
         end
         
+        % add moving conditions
+        for mm = 1:length(motion(freq).cond)
+            clear uniAxx
+            uniAxx = uniformAxx(Axx(motion(freq).cond(mm)));
+            save([axxFolder filesep 'Axx_c' num2str(20+mm,'%02d')],'-struct','uniAxx')
+        end
+            
+        
     end
     
 end
+
+
+%         % All the data that is loaded should be of the same dimensions
+%         for cc=1:length(Axx)
+%             sizeAxx(cc) = size(Axx(cc).wave,2);
+%         end
+%         if unique(sizeAxx) > 1
+%             waveMax = max(sizeAxx);
+%             for cc=1:length(Axx)
+%                 if size(Axx(cc).wave,2) ~= waveMax
+%                     nbCopy = waveMax / size(Axx(cc).wave,2);
+%                     Axx(cc).wave = repmat(Axx(cc).wave,1,nbCopy);
+%                     % need to replace nt as well
+%                     Axx(cc).nt = waveMax;
+%                     % I copy the time or should replace it?
+%                     Axx(cc).time = repmat(Axx(cc).time,1,nbCopy);
+%                 end
+%             end
+%         end
